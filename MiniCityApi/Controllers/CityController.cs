e4bbc30cccaf4e9beaffdfa741f53289ccd0a11d
yijie1206 <ylou351@aucklanduni.ac.nz>
2025-12-22 21:54:43 +1300

    Add project files.

---
+++ MiniCityApi/Controllers/CityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniCityApi.Model;
+
+namespace MiniCityApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            var cities = new List<CityDto>
+
+            {
+                new CityDto
+                {
+                Id = 1,
+                Name = "Auckland",
+                Description = "Mila's birth place"
+                },
+
+            new CityDto
+            {
+
+                Id = 2,
+                Name = "Beijing",
+                Description = "Capital city of China"
+            }};
+            return cities;
+
+        }
+    }
+
+}
