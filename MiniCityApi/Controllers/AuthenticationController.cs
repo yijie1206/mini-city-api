af3c50f9914509340f5ac030f465eab5862612f2
yijie1206 <ylou351@aucklanduni.ac.nz>
2026-01-10 08:52:07 +1300

    Prepare JWT signing key from configuration with fail-fast validation

--- MiniCityApi/Controllers/AuthenticationController.cs
+++ MiniCityApi/Controllers/AuthenticationController.cs
@@ -1,7 +1,9 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MiniCityApi.DTOs;
 using MiniCityApi.Services;
+using System.Text;
 
 
 
@@ -15,23 +17,28 @@ namespace MiniCityApi.Controllers
 
         private IAuthenticationService _authenticationService;
 
-        public AuthenticationController(IAuthenticationService authenticationService)
+        private IConfiguration _configuration;
+
+        public AuthenticationController(IAuthenticationService authenticationService, IConfiguration configuration)
         {
             _authenticationService = authenticationService;
-
+            _configuration = configuration;
         }
+
+
 
         [HttpPost("login")]
         public ActionResult<String> Login(AuthenticationRequestBodyDto authenticationRequestBody)
         {
+            var key = _configuration["Authentication:SecretForKey"];
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
             var validateCredential = _authenticationService.ValidateCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
-            if (validateCredential==null)
+            if (validateCredential == null)
             {
                 return Unauthorized();
             }
             return Ok();
         }
-
-
     }
 }
