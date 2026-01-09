45de319605788ecd1a4978372373018079f42651
yijie1206 <ylou351@aucklanduni.ac.nz>
2026-01-10 11:10:06 +1300

    Implement JWT token issuance in authentication controller

--- MiniCityApi/Controllers/AuthenticationController.cs
+++ MiniCityApi/Controllers/AuthenticationController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using MiniCityApi.DTOs;
 using MiniCityApi.Services;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 
@@ -37,13 +40,41 @@ namespace MiniCityApi.Controllers
             }
 
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
+            //validate input for authentication request body
             var validateCredential = _authenticationService.ValidateCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
             if (validateCredential == null)
             {
                 return Unauthorized();
             }
-            return Ok();
+
+            var claimForToken = new List<Claim>
+            {
+                new Claim("sub",validateCredential.UserId.ToString()),
+                new Claim("username", validateCredential.UserName),
+                new Claim("city",validateCredential.City),
+                new Claim("role", validateCredential.Role)
+            };
+
+            //create token object
+            var jwtSecurityToken = new JwtSecurityToken
+
+                (
+                   issuer: _configuration["Authentication:Issuer"],
+                   audience: _configuration["Authentication:Audience"],
+                   claims: claimForToken,
+                   expires: DateTime.UtcNow.AddHours(1),
+                   signingCredentials: signingCredentials
+
+                );
+
+            //convert token object to a JWT string 
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+
+
         }
     }
 }
