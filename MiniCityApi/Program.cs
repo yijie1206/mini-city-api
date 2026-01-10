ac2375845fe9ca33b659c72b8e3b80905f2ab15f
yijie1206 <ylou351@aucklanduni.ac.nz>
2026-01-10 22:19:26 +1300

    Configure JWT bearer authentication and token validation

--- MiniCityApi/Program.cs
+++ MiniCityApi/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Experimental;
 using MiniCityApi.Repositories;
+using System.Text;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +16,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<ICityRepository, InMemoryCityRepository>( );
+builder.Services.AddSingleton<ICityRepository, InMemoryCityRepository>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        var secretKey = builder.Configuration["Authentication:SecretForKey"]
+        ?? throw new InvalidOperationException("JWT SecretForKey is not configured");
+
+        options.TokenValidationParameters = new()
+        {   
+
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(secretKey)
+                )
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -21,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
