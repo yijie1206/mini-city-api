caa1d908663088dbac7c61be81b7d3122af5fa0a
yijie1206 <ylou351@aucklanduni.ac.nz>
2026-01-16 11:55:39 +1300

    Add EF Core migration and SQLite persistence

--- MiniCityApi/Data/CityDbContext.cs
+++ MiniCityApi/Data/CityDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace MiniCityApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MiniCityApi.DomainModel;
+
+namespace MiniCityApi.Data
 {
-    public class CityDbContext
+    public class CityDbContext : DbContext
     {
+        public DbSet<CityModel>Cities{get; set;}
+
+        public CityDbContext(DbContextOptions<CityDbContext>options):base (options)
+        { 
+        
+        }
     }
 }
---
+++ MiniCityApi/Migrations/20260115222522_CityModelDBInitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniCityApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CityModelDBInitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
