008cb0eb5253115b1f696414ddf65aaade69f522
yijie1206 <ylou351@aucklanduni.ac.nz>
2026-01-16 22:45:51 +1300

    refactor: remove legacy commented code from CityController

--- MiniCityApi/Controllers/CityController.cs
+++ MiniCityApi/Controllers/CityController.cs
@@ -16,36 +16,6 @@ namespace MiniCityApi.Controllers
     [ApiController]
     public class CityController : ControllerBase
     {
-
-
-        //private static List<CityModel> _citiesModel;
-
-        //public CityController()
-        //{
-        //    if (_citiesModel == null)
-        //    {
-
-        //        _citiesModel = new List<CityModel>
-        //    {
-        //        new CityModel
-        //        {
-        //        Id = 1,
-        //        Name = "Auckland",
-        //        Description = "New Zealand’s largest city"
-        //        },
-
-        //    new CityModel
-        //    {
-
-        //        Id = 2,
-        //        Name = "Beijing",
-        //        Description = "Capital city of China"
-        //    }};
-
-        //    }
-
-        //}
-
         private readonly ICityRepository _cityRepository;
         private readonly IMapper _mapper;
 
@@ -70,7 +40,6 @@ namespace MiniCityApi.Controllers
         [HttpGet("{cityId}")]
         public ActionResult<CityDto> GetCity(int cityId)
         {
-            //var city = _citiesModel.FirstOrDefault(c => c.Id == cityId);
             var city = _cityRepository.GetCity(cityId);
             if (city == null)
             {
@@ -83,53 +52,20 @@ namespace MiniCityApi.Controllers
             }
         }
 
+
         [HttpPost]
         public ActionResult<CityDto> CreateCity([FromBody] CreateCityDto createCityDto)
         {
 
-            //1.validate DTO
             if (string.IsNullOrWhiteSpace(createCityDto.Name))
             {
                 return BadRequest("City name is required.");
             }
 
-            //2.Build domain model
-            //declear cityModel variable so it can be used in AddCity method call below
-            //var cityModel = new CityModel
-            //{
-            //    Name = createCityDto.Name,
-            //    Description = createCityDto.Description,
-            //};
             var cityModel = _mapper.Map<CityModel>(createCityDto);
 
-            // 3. Call repository
             var city = _cityRepository.AddCity(cityModel);
-           
-            ////2.
-            //int maxId = 1;
-            //foreach (var c in _citiesModel)
-            //{
-            //    if (c.Id > maxId)
-            //    {
-            //        maxId = c.Id;
-            //    }
-            //}
-            //int newId = maxId + 1;
-            //3.
-            //can be improved this part.check GPT
-            //CityDto city = new CityDto();/
-            //city.Id = newId;
-            //city.Name = createCityDto.Name;
-            //city.Description = createCityDto.Description;
-            //var city = new CityModel
-            //{
-            //    Id = newId,
-            //    Name = createCityDto.Name,
-            //    Description = createCityDto.Description,
-            //};
-            ////4.
-            //_citiesModel.Add(city);
-            //5.
+
             var cityDto = _mapper.Map<CityDto>(city);
             return CreatedAtAction
                 (
@@ -141,59 +77,35 @@ namespace MiniCityApi.Controllers
 
 
         [HttpPut("{cityId}")]
-
         public IActionResult UpdateCity(int cityId, [FromBody] UpdateCityDto updateCityDto)
-        {
-            //validate input
+        {           
             if (string.IsNullOrEmpty(updateCityDto.Name))
             {
                 return BadRequest("City name is required.");
             }
 
-            var getExistingCity = _cityRepository.GetCity(cityId);        
-                
+            var getExistingCity = _cityRepository.GetCity(cityId);
+
             if (getExistingCity == null)
             {
                 return NotFound();
             }
 
-            var cityModel = _mapper.Map(updateCityDto, getExistingCity);
-
-            //manual map is replace by automapper
-            //var cityModel = new CityModel
-            //{
-            //    Id = cityId,
-            //    Name = updateCityDto.Name,
-            //    Description = updateCityDto.Description,
-            //};
-
-
-            //save
-             _cityRepository.UpdateCity(cityModel);
-            
-            //if (!city)
-            //{
-            //    return NotFound();
-            //}
+            var cityModel = _mapper.Map(updateCityDto, getExistingCity);        
+            _cityRepository.UpdateCity(cityModel);               
             return NoContent();
         }
 
 
 
         [HttpDelete("{cityId}")]
-
         public IActionResult DeleteCity(int cityId)
-        {
-            //var city = _citiesModel.FirstOrDefault(c => c.Id == cityId);
+        {           
             var city = _cityRepository.DeleteCity(cityId);
             if (city == false)
             {
                 return NotFound();
-            }
-            else
-                //{
-                //    _citiesModel.Remove(city);
-                //}
+            }                       
                 return NoContent();
         }
     }
